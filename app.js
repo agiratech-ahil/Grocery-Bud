61866bda065a74d34049594953359b2682439429
agiratech-ahil <ahil.singh@agiratech.com>
2022-04-27 17:41:47 +0530

    updated

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// window.addEventListener("load", () => {
 var list = [];
 var inp = document.getElementById("tarea");
 var todolist = document.getElementById("list");
@@ -49,10 +48,9 @@ function clearli() {
   list = [];
   showlist();
 }
-function filt() {
-  // let input = document.getElementById("idtextbox");
+function search() {
   let filter = inp.value.toUpperCase();
-  // let ul = document.getElementById("dplist");
+
   let li = document.getElementsByClassName("content");
   let edit = document.getElementById("edit");
   let deletei = document.getElementById("delete");
@@ -67,111 +65,3 @@ function filt() {
     }
   }
 }
-
-// });
-
-// var text = document.getElementById("tarea");
-// var button = document.getElementById("btn");
-// var lists = document.querySelector("#list");
-// var mainli = document.querySelector("#mainli");
-// var dlt = document.querySelector("#delete");
-
-// window.addEventListener("load", () => {
-//   const form = document.querySelector("#new-form");
-//   const input = document.querySelector("#tarea");
-//   const listel = document.querySelector("#tasks");
-
-//   form.addEventListener("submit", (e) => {
-//     e.preventDefault();
-
-//     const task = input.value;
-//     if (!task) {
-//       alert("Please enter a task");
-//       return;
-//     }
-//     const taskel = document.createElement("div");
-//     taskel.classList.add("task");
-//     const taskContent = document.createElement("div");
-
-//     taskContent.classList.add("content");
-//     // taskContent.innerText = task;
-//     taskel.appendChild(taskContent);
-
-//     const taskinput = document.createElement("input");
-//     taskinput.classList.add("tasktext");
-//     taskinput.type = "text";
-//     taskinput.value = task;
-//     taskinput.setAttribute("readonly", "readonly");
-//     taskContent.appendChild(taskinput);
-
-//     const taskactions = document.createElement("div");
-//     taskactions.classList.add("actions");
-
-//     const taskedit = document.createElement("button");
-//     taskedit.classList.add("edit");
-//     taskedit.innerHTML = "<i class='fa-solid fa-pen-to-square'>" + "</i>";
-
-//     const taskdelete = document.createsElement("button");
-//     taskdelete.classList.add("delete");
-//     taskdelete.innerHTML =
-//       "<i id='delete'  class='fa-solid fa-trash-can'>" + "</i>";
-
-//     taskactions.appendChild(taskedit);
-//     taskactions.appendChild(taskdelete);
-
-//     taskel.appendChild(taskactions);
-
-//     listel.appendChild(taskel);
-//     input.value = "";
-
-//     taskedit.addEventListener("click", () => {
-//       if (taskedit.innerText.toLowerCase() == "edit") {
-//         taskinput.removeAttribute("readonly");
-//         taskinput.focus();
-//         taskedit.innerText = "Save";
-//       } else {
-//         taskinput.setAttribute("readonly", "readonly");
-
-//         taskedit.innerHTML = "<i class='fa-solid fa-pen-to-square'>" + "</i>";
-//         taskedit.innerText = "Edit";
-//         // taskedit.innerHTML = "<i class='fa-solid fa-pen-to-square'>" + "</i>";
-//       }
-//     });
-//     taskdelete.addEventListener("click", () => {
-//       listel.removeChild(taskel);
-//     });
-//   });
-// });
-
-// // function display(i) {
-// //   lists.innerHTML +=
-// //     "<li id='mainli'>" +
-// //     text.value +)
-// //     "</li>" +
-// //     "<i id='edit' class='fa-solid fa-pen-to-square'>" +
-// //     "</i>" +
-// //     "<i id='delete' onclick='deleteli(i)' class='fa-solid fa-trash-can'>" +
-// //     "</i>";
-// //   console.log(lists);
-// // }
-
-// // function deleteli(i) {
-// //   var tasks = document.querySelectorAll("#delete");
-
-// //   for (i = 0; i < tasks.length; i++) {
-// //     tasks[i].onclick = function () {
-// //       lists.parentNode.removeChild(lists);
-// //     };
-// //   }
-// // }var btnContainer = document.getElementById("myBtnContainer");
-// var btns = btnContainer.getElementsByClassName("btn");
-// for (var i = 0; i < btns.length; i++) {
-//   btns[i].addEventListener("click", function() {
-//     var current = document.getElementsByClassName("active");
-//     current[0].className = current[0].className.replace(" active", "");
-//     this.className += " active";
-//   });
-// }
-// function clearli() {
-//   document.getElementById("tasks").innerHTML = "";
-// }
